1dd1e0ef6aadc3bb4e17220c58b6a0b77a3135fe
David DeCarmine <hworld@holo-world.com>
2017-06-08 18:10:43 -0600

    Some TODO items for Launcher

--- src/launcher.ts
+++ src/launcher.ts
@@ -3,6 +3,7 @@ import * as util from './util';
 
 export abstract class Launcher
 {
+	// TODO(ylivay): Should set the credentials file for now.
 	static async launch( localPackage: GameJolt.IGamePackage, ...executableArgs: string[] )
 	{
 		const dir = localPackage.install_dir;
@@ -20,7 +21,9 @@ export abstract class Launcher
 		return new LaunchInstance( await Runner.Instance.launchNew( args ) );
 	}
 
-	static async launchReattach( port: number, pid: number )
+	// TODO(ylivay): Should return a promise of the launch instance on
+	// successful attach, otherwise a promise rejection.
+	static async attach( port: number, pid: number )
 	{
 		return new LaunchInstance( new Runner.Instance( port, pid ) );
 	}
